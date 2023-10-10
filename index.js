e676b33f1dca5fdc7d39a31046d99af7228756ee
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-10 12:03:39 +0200

    Rename

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const tmi = require('tmi.js');
-const fs = require('fs');
-require('dotenv').config({path: __dirname + '/.env'});
+const { TWITCH_OAUTH_TOKEN, MONGODB_URL, PORT } = require('./config/secret.json');
 
-const vigie = {};
-let discuss = 0;
-let rules = 0;
+// Api
+const express = require("express");
+const app = express();
+const RateLimit = require('express-rate-limit');
+const cors = require("cors");
+const mongoose = require("mongoose");
 
+const limiter = RateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+});
+
+app.use(limiter);
+app.use(express.json());
+app.use(cors());
+
+// BDD
+mongoose.connect(MONGODB_URL);
+
+// Client
 const client = new tmi.Client({
   connection: {
     secure: true,
@@ -13,18 +28,43 @@ const client = new tmi.Client({
   },
   identity: {
     username: 'pastille_bot',
-    password: process.env.TWITCH_OAUTH_TOKEN
+    password: TWITCH_OAUTH_TOKEN
   },
-  channels: [ 'dark_bichon' ]
+  channels: [ 'nibiche' ]
 });
 
-client.connect();
+// ##### FIX ##### \\
+if (!String.prototype.endsWith) {
+  Object.defineProperty(String.prototype, 'endsWith', {
+      enumerable: false,
+      configurable: false,
+      writable: false,
+      value: function (searchString, position) {
+          position = position || this.length;
+          position = position - searchString.length;
+          var lastIndex = this.lastIndexOf(searchString);
+          return lastIndex !== -1 && lastIndex === position;
+      }
+  });
+}
 
+try {
+  app.get("/", (req, res) => { res.status(200).json({ message: "Bienvenue sur le Backend de Pastille Twitch" }); });
+  app.all("*", (req, res) => { res.status(404).json({ message: "This route do not exist" }); });
+  app.listen(PORT, () => { console.log(`API Server : 🚀 | Started on port ${PORT}`); });
+}
+catch(error) { console.log(error); }
+
+
+client.connect();
 client.on('message', (channel, tags, message, self) => {
   if(self) { return; }
 
   const args = message.slice(1).split(' ');
   const command = args.shift().toLowerCase();
+
+  let discuss = 0;
+  let rules = 0;
 
   if(!message.startsWith('!') && !self && rules < 9) { rules++; } // Tout les 10 messages hors bot et commandes on envoie ces messages 
   else if(!message.startsWith('!') && !self) {
@@ -97,7 +137,7 @@ client.on('message', (channel, tags, message, self) => {
           client.say(channel, `File découvrir lolytroly --> https://twitch.tv/lolytroly`);
           break;
         case 'social':
-          client.say(channel, `Retrouve moi un peu partout : Insta --> https://l.pooks.fr/instagram / Youtube --> https://l.pooks.fr/youtube / Discord --> https://l.pooks.fr/discord`);
+          client.say(channel, `Retrouve moi un peu partout : Insta --> https://instagram.com/meunierjeremie / Youtube --> https://www.youtube.com/channel/UCBoJiAGFyd7xg5YcVCoxDHA / Discord --> https://discord.gg/invite/QsUbmCBQ4N`);
           break;
       }
     }
@@ -105,20 +145,20 @@ client.on('message', (channel, tags, message, self) => {
     // public command
     switch (command) {
       case 'discord':
-        client.say(channel, `Retrouve nous sur le discord de BichonWood --> https://l.pooks.fr/discord`);
+        client.say(channel, `Retrouve nous sur le discord de BichonWood --> https://discord.gg/invite/QsUbmCBQ4N`);
         break;
       case 'youtube':
       case 'ytb':
       case 'yt':
       case 'replay':
-          client.say(channel, `Mes bétises sont aussi disponible sur youtube --> https://l.pooks.fr/youtube`);
+          client.say(channel, `Mes bétises sont aussi disponible sur youtube --> https://www.youtube.com/channel/UCBoJiAGFyd7xg5YcVCoxDHA`);
           break;
       case 'work':
       case 'quoiquonfait':
         client.say(channel, `On design le manoir de votre serviteur`);
         break;
       case 'planning':
-        client.say(channel, `Euh... on verra sinon c'est ici --> https://l.pooks.fr/planning`);
+        client.say(channel, `Euh... on verra 😊`);
         break;
       case 'deniche':
         client.say(channel, `DéNICHE est l'émission où je code, design ou découvre des sites et des technologies.`);
@@ -138,16 +178,16 @@ client.on('message', (channel, tags, message, self) => {
       //case 'seed':
       //  client.say(channel, `La seed est 10532435`);
       //  break;
-      case 'mods':
-        client.say(channel, `La liste des mods que j'utilise : InGameStats, Map Tooltip, Shulker Tooltip, Replay Mod, Simple voice Chat, Visible Barriers, Tweakeroo, Sound Physics Remastered et Xaero's World Map`);
-        break;
-      case 'cmds':
-      case 'commands':
-      case 'cmd':
-      case 'commandes':
-      case 'commande':
-        client.say(channel, `Voici la liste des commandes, ajoute un "!" devant : discord, notifs, replay, join, planning, youtube, quoiquonfait. Retrouve les aussi sur : https://pastille.jeremiemeunier.fr/twitch#commands`);
-        break;
+      // case 'mods':
+      //   client.say(channel, `La liste des mods que j'utilise : InGameStats, Map Tooltip, Shulker Tooltip, Replay Mod, Simple voice Chat, Visible Barriers, Tweakeroo, Sound Physics Remastered et Xaero's World Map`);
+      //   break;
+      // case 'cmds':
+      // case 'commands':
+      // case 'cmd':
+      // case 'commandes':
+      // case 'commande':
+      //   client.say(channel, `Voici la liste des commandes, ajoute un "!" devant : discord, notifs, replay, join, planning, youtube, quoiquonfait. Retrouve les aussi sur : https://pastille.jeremiemeunier.fr/twitch#commands`);
+      //   break;
       case 'relou':
         client.say(channel, `🚨 ALERTE RELOU ! 🚨 Mise en place du place VIGIRELOU !`);
         break;
